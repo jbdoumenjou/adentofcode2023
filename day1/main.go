6e1e8863069f8d74d137ca54f29ef1dfd8328f72
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-12-01 22:07:21 +0100

    day 1 part 1

---
+++ day1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var result int
+
+	for scanner.Scan() {
+		content := scanner.Text()
+		result += getSum(content)
+	}
+
+	fmt.Println("Result: ", result)
+}
+
+func getSum(s string) int {
+	if len(s) == 0 || s == "\n" {
+		return 0
+	}
+
+	firstIdx := strings.IndexFunc(s, unicode.IsDigit)
+	first := int(s[firstIdx] - '0')
+
+	lastIdx := strings.LastIndexFunc(s, unicode.IsDigit)
+	last := int(s[lastIdx] - '0')
+
+	return first*10 + last
+}
