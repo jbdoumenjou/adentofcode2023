f1646f7697c861fce8f7ab4da59f4abbe9f0a2ab
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-12-02 10:09:03 +0100

    day 2 part 2

--- day2/part1.go
+++ day2/part1.go
@@ -9,27 +9,32 @@ import (
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("part2-input.txt")
+func part1() {
+	file, err := os.Open("part1-input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	var powerSum int
+	bag := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
 	scanner := bufio.NewScanner(file)
-
+	i := 1
+	var sum int
 	for scanner.Scan() {
 		content := scanner.Text()
-		powerSum += powerOfCubes(content)
-	}
+		if isPossibleGame(content, bag) {
+			sum += i
+		}
 
-	fmt.Println(powerSum)
+		i++
+	}
+	fmt.Println(sum)
 }
 
-func powerOfCubes(s string) int {
-	minBag := make(map[string]int)
+func isPossibleGame(s string, bag map[string]int) bool {
 	// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-
 	game := strings.Split(s, ":")
 	// 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 	sets := strings.Split(game[1], ";")
@@ -44,16 +49,11 @@ func powerOfCubes(s string) int {
 				log.Fatal(err)
 			}
 
-			if val > minBag[elt[1]] {
-				minBag[elt[1]] = val
+			if val > bag[elt[1]] {
+				return false
 			}
 		}
 	}
 
-	power := 1
-	for _, v := range minBag {
-		power *= v
-	}
-
-	return power
+	return true
 }
