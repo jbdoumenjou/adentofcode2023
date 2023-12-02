9586dd04cfa04daa14a13bbee2d66b25e95c948f
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-12-02 08:58:36 +0100

    day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -6,37 +6,73 @@ import (
 	"log"
 	"os"
 	"strings"
-	"unicode"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
+	part2()
+}
+
+func part2() {
+	//file, err := os.Open("part2-example.txt")
+	file, err := os.Open("part2-input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	keys := map[string]int{
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9}
 	var result int
 
 	for scanner.Scan() {
 		content := scanner.Text()
-		result += getSum(content)
+		sum := getSum2(content, keys)
+		fmt.Println(sum)
+		result += sum
 	}
 
 	fmt.Println("Result: ", result)
 }
 
-func getSum(s string) int {
+func getSum2(s string, keys map[string]int) int {
 	if len(s) == 0 || s == "\n" {
 		return 0
 	}
 
-	firstIdx := strings.IndexFunc(s, unicode.IsDigit)
-	first := int(s[firstIdx] - '0')
+	firstIdx, firstVal, lastIdx, lastVal := -1, -1, -1, -1
+	for key, i := range keys {
+		if index := strings.Index(s, key); index != -1 {
+			if firstIdx == -1 || index < firstIdx {
+				firstIdx = index
+				firstVal = i
+			}
+		}
+		if index := strings.LastIndex(s, key); index != -1 {
+			if lastIdx == -1 || index > lastIdx {
+				lastIdx = index
+				lastVal = i
+			}
+		}
 
-	lastIdx := strings.LastIndexFunc(s, unicode.IsDigit)
-	last := int(s[lastIdx] - '0')
+	}
 
-	return first*10 + last
+	return firstVal*10 + lastVal
 }
