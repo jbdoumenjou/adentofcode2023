ebf2e94c3b6fbddfc51c36e29dd12537a1acca41
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2023-12-27 10:23:23 +0100

    day 6 part 1

---
+++ day6/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// part1: 5133600
+func main() {
+	entries := getEntries("part1-input.txt")
+	races := getRaces(entries)
+
+	fmt.Println(getResult(races))
+}
+
+func getResult(races []race) int {
+	var result = 1
+
+	for _, race := range races {
+		result *= race.nbOfWaysToBeatDistance()
+	}
+
+	return result
+}
+
+type race struct {
+	Time     int
+	Distance int
+}
+
+func (r race) nbOfWaysToBeatDistance() int {
+	var nbWays int
+
+	for i := 1; i < r.Time-1; i++ {
+		if (r.Time-i)*i > r.Distance {
+			nbWays++
+		}
+	}
+
+	return nbWays
+}
+
+func getEntries(path string) []string {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var entries []string
+	for scanner.Scan() {
+		entries = append(entries, scanner.Text())
+	}
+
+	return entries
+}
+
+func getRaces(entries []string) []race {
+	times := getValues(entries[0])
+	distances := getValues(entries[1])
+
+	var races []race
+	for i := 0; i < len(times); i++ {
+		races = append(races, race{
+			Time:     times[i],
+			Distance: distances[i],
+		})
+	}
+
+	return races
+}
+
+func getValues(s string) []int {
+	var times []int
+
+	line := strings.Split(s, ":")
+	values := strings.Split(line[1], " ")
+	for _, value := range values {
+		if value == "" {
+			continue
+		}
+		times = append(times, toInt(value))
+	}
+
+	return times
+}
+
+func toInt(s string) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return val
+}
---
+++ day6/main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_nbOfWaysToBeatDistance(t *testing.T) {
+
+	tests := []struct {
+		name     string
+		r        race
+		time     int
+		distance int
+		want     int
+	}{
+		{
+			name: "no way",
+			r: race{
+				Time:     1,
+				Distance: 1,
+			},
+			want: 0,
+		},
+		{
+			name: "simplest",
+			r: race{
+				Time:     3,
+				Distance: 1,
+			},
+			want: 1,
+		},
+		{
+			name: "first line of example",
+			r: race{
+				Time:     7,
+				Distance: 9,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.r.nbOfWaysToBeatDistance(); got != tt.want {
+				t.Errorf("nbOfWaysToBeatDistance() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getRaces(t *testing.T) {
+
+	entries := []string{
+		"Time:      7  15   30",
+		"Distance:  9  40  200",
+	}
+	want := []race{
+		{Time: 7, Distance: 9},
+		{Time: 15, Distance: 40},
+		{Time: 30, Distance: 200},
+	}
+	if got := getRaces(entries); !reflect.DeepEqual(got, want) {
+		t.Errorf("getEntries() = %v, want %v", got, want)
+	}
+
+}
